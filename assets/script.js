0d9ba9d504ba22befff1b65267ed81581e798c72
Istvan Szalontai <istvan.szalontai@accenture.com>
2025-02-23 20:41:30 +0100

    arak feladat added

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 const map1 = new Map()
 map1.set("1", ["1986", "./novenyes/novenyes_feladat.pdf"])
 map1.set("2", ["1986", "./novenyes/novenyek_forras.txt"])
+map1.set("3", ["2112", "./arak/arak_feladat.pdf"])
+map1.set("4", ["2112", "./arak/arak_nyers.xlsx"])
 var current_obj = null
 
 function unlock(obj) {
---
+++ excel/arak/jegyzetek.md
@@ -0,0 +1,1 @@
+https://www.ksh.hu/stadat_files/ara/hu/ara0044.html
