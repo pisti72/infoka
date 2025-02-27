f45f787734ee7918e9df81d4c3b4f7a1f5640615
Istvan Szalontai <istvan.szalontai12@gmail.com>
2025-02-27 12:51:18 +0100

    upload added

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 const map1 = new Map()
-map1.set("1", ["1986", "./novenyes/novenyes_feladat.pdf"])
-map1.set("2", ["1986", "./novenyes/novenyek_forras.txt"])
-map1.set("3", ["2112", "./arak/arak_feladat.pdf"])
-map1.set("4", ["2112", "./arak/arak_nyers.xlsx"])
-map1.set("5", ["7895", "./ittas/baleseti_statisztika_feladat.pdf"])
-map1.set("6", ["7895", "./ittas/baleseti_statisztika_forras.xlsx"])
-map1.set("7", ["5456", "./lotto5/lotto5_feladat.pdf"])
-map1.set("8", ["5456", "./lotto5/lotto5_forras.xlsx"])
+map1.set("1", ["1986", "excel/novenyes/novenyes_feladat.pdf"])
+map1.set("2", ["1986", "excel/novenyes/novenyek_forras.txt"])
+map1.set("3", ["2112", "excel/arak/arak_feladat.pdf"])
+map1.set("4", ["2112", "excel/arak/arak_nyers.xlsx"])
+map1.set("5", ["7895", "excel/ittas/baleseti_statisztika_feladat.pdf"])
+map1.set("6", ["7895", "excel/ittas/baleseti_statisztika_forras.xlsx"])
+map1.set("7", ["5456", "excel/lotto5/lotto5_feladat.pdf"])
+map1.set("8", ["5456", "excel/lotto5/lotto5_forras.xlsx"])
 var current_obj = null
 
 function unlock(obj) {
@@ -15,7 +15,7 @@ function unlock(obj) {
     current_obj = obj
     if (current_obj.href.length > 0) {
         doorkeeper.style.display = "none"
-    }else{
+    } else {
         pin = document.getElementById("pin")
         pin.focus();
     }
